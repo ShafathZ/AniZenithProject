af85a8e759a133f4292f31458aedafd08af269e4
ShafathZ <53407653+ShafathZ@users.noreply.github.com>
2026-02-03 16:36:11 -0500

    Merged UI changes and ensured full pipeline is working

--- backend_adapter.py
+++ backend_adapter.py
@@ -3,14 +3,16 @@ import backend
 
 
 # Adapter function between frontend and backend. Returns a generator yielding backend results.
+
 def respond(
     message,
     history: list[dict[str, str]],
     system_message,
+    use_local_model,
     max_tokens,
     temperature,
     top_p,
     hf_token: gr.OAuthToken,
 ):
-    for r in backend.process_user_query(system_message, history, message, True, max_tokens, temperature, top_p, hf_token):
+    for r in backend.process_user_query(system_message, history, message, use_local_model, max_tokens, temperature, top_p, hf_token):
         yield r
