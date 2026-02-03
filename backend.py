0368299942263207c2c28cc5b7411cd85ddaf142
ShafathZ <53407653+ShafathZ@users.noreply.github.com>
2026-02-03 15:06:10 -0500

    Completed stub code for backend general pipeline. Still need detect_genres function, but requires genrelist.txt file

--- backend.py
+++ backend.py
@@ -1,4 +1,26 @@
+from typing import List
+
+from retrieval_utils import get_recommendations
+
+def detect_genres(message: str) -> List[str]:
+    # TODO: Detect genres from a genrelist.txt file. If message contains any, append that as genre to the list.
+    pass
+
+def query_model(system_message, history, user_message, use_local_model, max_tokens, temperature, top_p, hf_token):
+    # TODO: Program calling model using Amaan's code
+    pass
 
 def process_user_query(system_message, history, user_message, use_local_model, max_tokens, temperature, top_p, hf_token):
+    # 1. Retrieve genres from the user message using naive approach
     genre_list = detect_genres(user_message)
-    recommendations = get_recommendations()
+
+    # 2. Retrieve relevant results from DB
+    recommendations_string = get_recommendations(genre_list)
+
+    # 3. Append recommendation string to system message
+    system_message = system_message + recommendations_string
+
+    # 4. Query the model
+    for result in query_model(system_message, history, user_message, use_local_model, max_tokens, temperature, top_p, hf_token):
+        yield result
+
