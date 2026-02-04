f119eb21453589774ea9f76e6fc07692d8fe61f4
Suryansh Goyal <suryanshg47@gmail.com>
2026-02-04 17:25:42 -0500

    Added System prompt for making sure the Local Model works a bit better for anime recommendations, but we are still not there

--- backend.py
+++ backend.py
@@ -1,28 +1,43 @@
 from typing import List
-
 from huggingface_hub import InferenceClient
 from transformers import pipeline
-
 from retrieval_utils import get_recommendations
 
 genre_list = open("genrelist.txt", "r").read().splitlines()
 
+SYSTEM_PROMPT = f"""
+You are an expert on recommending Anime shows. Please use the RECOMMENDATIONS to answer the user's question.
+The RECOMMENDATIONS is a JSON String that contains information of top Anime sorted in descending order by:
+1. Number of Requested Genre Matches from the User
+2. The Score of the Anime
+
+If the RECOMMENDATIONS JSON String is not given: 
+1. Then answer the question like a Friendly Chatbot!
+2. Do not reference anything about a RECOMMENDATION JSON
+3. Ask the user to provide their favorite genre(s) for Anime Recommendations
+"""
+
 def process_user_query(system_message: str, history: List[dict], user_message: str, use_local_model: bool, max_tokens: int, temperature: float, top_p: float, hf_token):
     # 1. Retrieve genres from the user message using naive approach
     genre_list = detect_genres(user_message)
-    #print(f"Requested genres: {genre_list}")
 
-    # 2. Retrieve relevant results from DB
-    recommendations_string = get_recommendations(genre_list)
-    #print(f"Recommendations found: {recommendations_string}")
+    # 2. Retrieve relevant results from DB if the genre_list is not empty
+    recommendations_string = ""
+    if len(genre_list) > 0:
+        recommendations_string = get_recommendations(genre_list)
 
-    # 3. Append recommendation string to system message
-    # TODO: Use few-shot conversation method instead of appending to system prompt for better results
-    system_message = system_message + recommendations_string
-
-    # 4. Query the model
-    for result in query_model(system_message, history, user_message, use_local_model, max_tokens, temperature, top_p, hf_token):
+    # 3. Query the model
+    for result in query_model(system_message, 
+                              history, 
+                              user_message,
+                              recommendations_string, 
+                              use_local_model, 
+                              max_tokens, 
+                              temperature, 
+                              top_p, 
+                              hf_token):
         yield result
+
 
 def detect_genres(message: str) -> List[str]:
     requested_genres = []
@@ -33,34 +48,48 @@ def detect_genres(message: str) -> List[str]:
             requested_genres.append(genre)
     return requested_genres
 
+
 def query_model(
         system_message: str,
         history: List[dict],
         user_message: str,
+        recommendations_string: str,
         use_local_model: bool,
         max_tokens: int,
         temperature: float,
         top_p: float,
         hf_token):
-    # Construct prompt for language model
-    messages = [{"role": "system", "content": system_message}]
+    
+    # Construct messages for the language model
+    # Start by adding system prompt
+    system_prompt = SYSTEM_PROMPT
+    if recommendations_string:
+        system_prompt += "\nRECOMMENDATION JSON:" + f"\n{recommendations_string}"
+    messages = [{"role": "system", "content": system_prompt}]
+
+    # Add the rest of the history
     messages.extend(history)
+
+    # Add the current user prompt
     messages.append({"role": "user", "content": user_message})
 
     # Determine which model to use (local or external)
     if use_local_model:
-        # Local Model -- Use pipeline
-        # TODO: Change to non-thinking model since it does not make sense for our application currently
-        pipe_liquid = pipeline(task='text-generation',
-                               model='LiquidAI/LFM2.5-1.2B-Thinking',
+        # Local Model -- Uses pipeline from transformers library
+        pipeline_local_model = pipeline(task='text-generation',
+                               model='google/gemma-3-1b-it',
                                max_new_tokens=max_tokens,
                                temperature=temperature,
                                do_sample=False,
-                               top_p=top_p)
-        response = pipe_liquid(messages)
-        # TODO: Once non-thinking model is implemented, streaming can be added. If we decide to keep thinking model, streaming needs delay to first End-Think token
-        response_string = response[0]['generated_text'][-1]['content'].split('</think>')[-1].strip()
-        yield response_string
+                               top_p=top_p
+                               )
+        # Get the response from the local model
+        response = pipeline_local_model(messages)
+        
+        # Parse the output and yield it
+        yield response[0]['generated_text'][-1]['content'].strip()
+            
+
     elif not use_local_model:
         # Non-local Model -- Use InferenceClient
         client = InferenceClient(
