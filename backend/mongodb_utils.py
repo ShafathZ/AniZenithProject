2cdfcde7b3d12589cb0e362e18252c0510308d57
Suryansh Goyal <suryanshg47@gmail.com>
2026-04-10 15:22:19 -0400

    Saving progress on MongoDB utils, need tests and integration to the backend code

---
+++ backend/mongodb_utils.py
@@ -0,0 +1,99 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from sentence_transformers import SentenceTransformer
+from pydantic import BaseModel
+from typing import List, Dict
+
+
+# Class to Model a typical Anime Document
+class AnimeDocument(BaseModel):
+    name: str
+    score: float
+    synopsis: str
+    genres: List[str]
+
+
+# Class to model Anizenith MongoDB Client related utilities
+class AniZenithMongoClient:
+    def __init__(self, conn_string):
+        self.embedding_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+        self.db_client = MongoClient(conn_string)
+        self.anime_collection = self.db_client["anizenith"]["anime"]
+
+
+    def add_anime(self, anime_document: AnimeDocument) -> None:
+        # Create text_metadata field using synopsis, genres and name
+        text_metadata = f"Synopsis: {anime_document.synopsis}\n\nGenres: {', '.join(anime_document.genres)}\n\nName: {anime_document.name}"
+        
+        # Create embedding for the text_metadata field
+        text_metadata_embedding = self.embedding_model.encode(text_metadata)
+
+        # Create a new document to be inserted into MongoDB
+        anime_document_dict = {
+            "name": anime_document.name,
+            "score": anime_document.score,
+            "synopsis": anime_document.synopsis,
+            "genres": anime_document.genres,
+            "text_metadata": text_metadata,
+            "text_metadata_embedding": text_metadata_embedding
+        }
+
+        # Insert the new document
+        self.anime_collection.insert_one(anime_document_dict)
+
+
+    def execute_mongo_read_query(self, query, limit = None) -> List[Dict]:
+        try:
+            if isinstance(query, dict):
+                # Execute a standard find operation (e.g., {"score": {"$gt": 8.0}})
+                cursor = self.anime_collection.find(query)
+                if limit is not None:
+                    cursor = cursor.limit(limit)
+
+            else:
+                raise ValueError("Query must be a dict (filter)")
+            
+            # Convert the cursor to a list of dictionaries to return
+            return list(cursor)
+        
+        # Catch any Exception that happened
+        except Exception as e:
+            print(f"Error executing MongoDB query: {e}")
+            return []
+
+
+    def perform_vector_search(self, user_query: str, limit: int = 5, num_candidates: int = 100):
+         # Embed the search query
+        query_vector = self.embedding_model.encode(user_query).tolist()
+        
+        # Define the $vectorSearch aggregation pipeline
+        pipeline = [
+            {
+                "$vectorSearch": {
+                    "index": "vector_index",                  
+                    "path": "text_metadata_embedding",       
+                    "queryVector": query_vector,
+
+                    # Number of candidates to evaluate (must be >= limit)  
+                    # Uses HNSW           
+                    "numCandidates": num_candidates,                     
+                    "limit": limit                              
+                }
+            },
+            {
+                # Project only the fields we want to see, plus the search score
+                "$project": {
+                    "_id": 0,
+                    "name": 1,
+                    "genres": 1,
+                    "score": 1,
+                    "synopsis": 1,
+                    "similarity_score": {"$meta": "vectorSearchScore"}         # Include the similarity score
+                }
+            },
+        ]
+        
+        # Execute the pipeline
+        results = self.anime_collection.aggregate(pipeline)
+        
+        return results
--- data/populate_mongo_db.py
+++ data/populate_mongo_db.py
@@ -2,9 +2,15 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 import pandas as pd
+from sentence_transformers import SentenceTransformer
+from pymongo.operations import SearchIndexModel
+
 
 # Load all Env variables
 load_dotenv()
+
+# Load Sentence Transformer Model
+embedding_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 # Load the Anime Dataset as a Pandas DataFrame
 df = pd.read_csv('data/anime-dataset-2023.csv')
@@ -38,12 +44,16 @@ anime_df['Genres'] = anime_df['Genres'].apply(lambda x: [g.strip() for g in str(
 
 # Create a new field called text_metadata
 anime_df['text_metadata'] = anime_df.apply(
-    lambda row: f"Name: {row['English name']}\n\nGenres: {', '.join(row['Genres'])}\n\nSynopsis: {row['Synopsis'].replace('\n', '')}",
+    lambda row: f"Synopsis: {row['Synopsis'].replace('\n', '')}\n\nGenres: {', '.join(row['Genres'])}\n\nName: {row['English name']}",
     axis = 1
 )
 
+# Embed the text_metadata field
+print("Generating embeddings... this might take a minute...")
+anime_df['text_metadata_embedding'] = anime_df['text_metadata'].apply(lambda x: embedding_model.encode(x).tolist())
+
 # Keep only the columns we need and rename them
-anime_df = anime_df[['English name', 'Score', 'Synopsis', 'Genres', 'text_metadata']]
+anime_df = anime_df[['English name', 'Score', 'Synopsis', 'Genres', 'text_metadata', 'text_metadata_embedding']]
 print(anime_df.head())
 
 anime_df = anime_df.rename(columns={
@@ -73,6 +83,27 @@ collection.delete_many({})
 
 # Insert the documents into the collection
 if documents:
+
+    # Define the Vector Search index
+    search_index_model = SearchIndexModel(
+        definition={
+            "fields": [
+                {
+                    "type": "vector",
+                    "path": "text_metadata_embedding",
+                    "numDimensions": embedding_model.get_embedding_dimension(),
+                    "similarity": "cosine"    # Options: "cosine", "dotProduct", "euclidean"
+                }
+            ]
+        },
+        name="vector_index",
+        type="vectorSearch"
+    )
+
+    # Create the index on your collection
+    result = collection.create_search_index(model=search_index_model)
+    print(f"Index creation started: {result}")
+
     result = collection.insert_many(documents)
     print(f"Successfully inserted {len(result.inserted_ids)} anime documents.")
 else:
