4ae5e2eb7754d44c21163c817de962dfb9d85959
ShafathZ <shafath.zaman@gmail.com>
2026-04-26 01:00:42 -0400

    Reverted more commit changes

--- backend/mongo/AniZenithVectorSearchResult.py
+++ backend/mongo/AniZenithVectorSearchResult.py
@@ -3,7 +3,7 @@ from typing import List
 
 # Class to Model a typical AniZenith Vector Search Result Object
 class AniZenithVectorSearchResult(BaseModel):
-    title: str = Field(..., min_length=1, description="The english name of the anime")
+    name: str = Field(..., min_length=1, description="The name of the anime")
     genres: List[str] = Field(default_factory=list, description="List of genres")
     score: float = Field(..., ge=0.0, description="The score of the anime (0 or higher)")
     synopsis: str = Field(..., description="The synopsis of the anime")
