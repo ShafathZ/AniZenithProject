48b99798c9cf09aa88316eace1906cf0702de1ef
ShafathZ <shafath.zaman@gmail.com>
2026-04-25 23:35:20 -0400

    Modified AnimeDocument for test case functions to work

--- data/anime_scrape.py
+++ data/anime_scrape.py
@@ -4,9 +4,9 @@ from typing import List, Dict
 
 import pandas as pd
 import requests
-from pydantic import BaseModel
 
 from backend.configs import backend_app_config
+from backend.mongo.AnimeDocument import AnimeDocument
 
 # Jikan is a web scraping REST API for anime data, but not all data can be available (specific anime search 100% uptime)
 JIKAN_ENDPOINT = "https://api.jikan.moe/v4/anime"
@@ -23,23 +23,6 @@ MAL_MAX_PER_PAGE = 100
 
 # Used for data cleaning
 DEMOGRAPHIC_GENRES = {"Shounen", "Shoujo", "Seinen", "Josei", "Kids", "Demographic", "Shonen", "Shojo"}
-
-class AnimeDocument(BaseModel):
-    mal_id: int                     # ID of anime on MyAnimeList
-    title: str                      # Title of anime (node) -- May not be English, but always matches recommendations title
-    alt_titles: Dict[str, str]      # { "en": ENGLISH_TITLE, "jp": JAPANESE_TITLE, ...}
-    score: float                    # MAL User Mean Score -- Typically 6-10
-    synopsis: str                   # Short synopsis of shows, does not contain spoilers beyond first episode
-    genres: List[str]               # Genres list not including demographic genre
-    demographic: str                # Primary demographic
-    age_rating: str                 # g | pg | pg-13 | r | r+ | rx
-    cover_image_url: str            # link to MAL image (not CDN)
-    date_aired: str                 # date aired in YYYY-MM-DD
-    status: str                     # finished_airing | currently_airing | not_aired
-    episode_count: int              # number of episodes in the anime
-    avg_episode_len_mins: int       # average duration per episode in mins
-    publishing_company: str         # publishing company name | Unknown
-    recommendations: Dict[str, int] # {"TITLE1":#_OF_RECOMMENDATIONS, "TITLE2":#_OF_RECOMMENDATIONS, ...}
 
 def _get_jikan_recommendations(mal_id: int, limit: int = 10) -> Dict[str, int]:
     """Fetch anime user recommendations from Jikan."""
